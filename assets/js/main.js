ff02eeee6157e4f4ffec23619a5b070b4eecdbe2
Iconic Robbott <dev.robbott@gmail.com>
2025-06-29 08:02:18 +0100

    Completed nav bar design for large screens

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const glow = document.querySelector('.cursor-glow');
+    const glow = document.querySelector('.cursor');
 
     if (glow) {
+        // Move the custom cursor
         document.addEventListener('mousemove', (e) => {
             glow.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+        });
+
+        // Handle hover effect on links
+        const links = document.querySelectorAll('a');
+        links.forEach(link => {
+            link.addEventListener('mouseenter', () => {
+                glow.classList.add('hovering-link');
+            });
+            link.addEventListener('mouseleave', () => {
+                glow.classList.remove('hovering-link');
+            });
         });
     }
 });
